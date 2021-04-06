45b5c46f3127f48d36f43b7ce041ca2dba6b8ba3
Ferdinand Cuisin <fcuisin@gmail.com>
2021-04-06 18:51:38 +0200

    Init Next.js App

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import styles from "../styles/Header.module.css"
+
+export default function Header() {
+  return (
+
+    <div className={styles.header}>
+      <h1>Hi, I'm Ferdinand Cuisin</h1>
+      <p>Junior Web Developper</p>
+    </div>
+
+  )
+}
