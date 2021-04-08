7c814acc21d1127f2278deff5ae742696edf2e06
Ferdinand Cuisin <fcuisin@gmail.com>
2021-04-08 22:53:02 +0200

    Init hero section

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,18 @@ import styles from "../styles/Header.module.css"
 
 export default function Header() {
   return (
-
+    <>
     <div className={styles.header}>
-      <h1>Hi, I'm Ferdinand Cuisin</h1>
-      <p>Junior Web Developper</p>
+      <div className={styles.leftContainer}>
+        <a href="/" className={styles.logo}>&#60;fc.  &#62;</a>
+      </div>
+      <div className={styles.rightContainer}>
+        <h1 className={styles.title}>Hi, I'm Ferdinand Cuisin</h1>
+        <p>Junior Web Developper</p>
+      </div>
     </div>
-
+    <div className={styles.header}>
+    </div>
+    </>
   )
 }
